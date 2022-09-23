1adb6c30c97bff52fb1fc9fbb25593a372381fde
Ilyasov <84683017+ibrahimbarood@users.noreply.github.com>
2022-09-23 18:25:32 +0200

    login

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>Barood - Login</title>
+  </head>
+<body>
+
+<center><img src="https://techcampus.sfo2.digitaloceanspaces.com/a2a2234f3a95f30212106352930286fe.png" width="100px"></center>
+
+<form action="login.php" method="post">
+🔐 Token: <input type="password" name="pass">
+<br><br>
+<input type="submit" value="Enter">
+</form>
+
+<!-- TechCampus copyright - Start -->
+<center><a style="text-decoration: none;color:black;font-size:14px;" href="http://barood.us" target="_blank"><img src="https://techcampus.sfo2.digitaloceanspaces.com/4f79892d3832f12c681344b9efdfcbfb.png" width="50px"><br>
+Barood - Beaming</a></center>
+<!-- TechCampus copyright - End -->
+
+</body>
+</html>
